1ffdc813343c694d03f8e49ca347bb6801e2666a
Pedro Ferreira <pedroferreiracjr@gmail.com>
2020-08-20 04:08:56 -0300

    initial commit

---
+++ client.c
@@ -0,0 +1,45 @@
+// Client side C/C++ program to demonstrate Socket programming 
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+#define PORT 8080
+
+int main(int argc, char const *argv[]) {
+
+	int sock = 0, valread;
+	struct sockaddr_in serv_addr;
+	char *hello = "Hello from client";
+	char buffer[1024] = {0};
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	// Convert IPv4 and IPv6 addresses from text to binary form
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+
+	char input[1024];
+	memset(input, 0, sizeof(input));
+	while (input > 0) {
+		memset(input, 0, sizeof(input));
+		printf("Type something: ");
+		fgets(input, sizeof(input), stdin);
+		send(sock, input, strlen(input), 0);
+	}
+
+	return 0;
+}
